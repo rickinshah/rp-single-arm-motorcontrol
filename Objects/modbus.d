41f58855390d72b3d2faaeda8a9ae8b3f8661e2b
Rickin Shah <rickinshah@proton.me>
2026-02-28 20:27:57 +0530

    remove tracking binaries

--- Objects/modbus.d
+++
@@ -1,2 +0,0 @@
-./objects/modbus.o: modbus.c modbus.h \
-  C:\Keil_v5\ARM\ARMCLANG\bin\..\include\stdint.h delay.h uart.h
--- Objects/modbus_ascii.d
+++
@@ -1,3 +0,0 @@
-./objects/modbus_ascii.o: modbus_ascii.c modbus_ascii.h \
-  C:\Keil_v5\ARM\ARMCLANG\bin\..\include\stdint.h rmcs_registers.h \
-  uart.h
